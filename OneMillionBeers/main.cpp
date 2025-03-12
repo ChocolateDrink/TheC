f3f33cdd51041675336f6079973ac8c910c5eb8e
chocolate <readonlychocolate@gmail.com>
2025-03-12 14:00:23 +0800

    Add files via upload

---
+++ OneMillionBeers/main.cpp
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include "./texts.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	char path[MAX_PATH] = {};
+	GetModuleFileNameA(NULL, path, MAX_PATH);
+
+	HKEY handle = NULL;
+	LONG result = RegOpenKeyExA(
+		HKEY_CURRENT_USER,
+		"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
+		NULL, KEY_SET_VALUE, &handle
+	);
+
+	if (result == ERROR_SUCCESS)
+		RegSetValueExA(handle, "Beer Alarm", NULL, REG_SZ, (CONST BYTE*)path, (DWORD)(strlen(path) + 1));
+
+	RegCloseKey(handle);
+
+	bool waited = false;
+
+	while (true) {
+		if (!waited) {
+			int sleepTime = texts::getRandomInt();
+			Sleep(sleepTime);
+
+			waited = true;
+		}
+
+		MBText textData = texts::getRandom();
+		MessageBox(NULL, textData.text, textData.title, MB_OK | MB_OKCANCEL);
+
+		int sleepTime = texts::getRandomInt();
+		Sleep(sleepTime);
+	}
+}
